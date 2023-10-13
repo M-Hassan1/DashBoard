fe5eb92157e4b6d146a1e18d80f173fc96aae9ee
M-Hassan1 <hassan81932@gmail.com>
2023-10-13 18:02:16 +0500

    Added Search Bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from '@/component/view/Navbar'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
--- src/component/view/Navbar/index.tsx
+++ src/component/view/Navbar/index.tsx
@@ -37,15 +37,15 @@ const Navbar = () => {
         </ul>
         <button
           onClick={toggleSearchBar}
-          className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center bg-gray-300 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
         >
           Search
           <BsArrowRightShort />
         </button>
       </div>
       <div
-        className={`fixed top-0 left-0 w-full h-12 transition-all duration-500 transform ${
-          isSearchBarVisible ? 'translate-x-0 visible' : '-translate-x-full invisible'
+        className={`fixed top-0 right-0 w-full h-12 transition-all duration-500 transform ${
+          isSearchBarVisible ? 'translate-x-0 visible' : 'translate-x-full invisible'
         }`}
       >
         <SearchBar hideSearch={() => setIsSearchBarVisible(false)} />
